5bb140cd48fae1c65b642abd567ccef6d91f437a
Austine Samuel <austinesamuel914@gmail.com>
2024-02-11 00:54:34 +0100

    profile ...

--- src/components/pages/profile.jsx
+++ src/components/pages/profile.jsx
@@ -1,39 +1,62 @@
-import React from 'react';
-import {mockUser as user} from '../_mock/user.js';
+import React, { useEffect, useState } from 'react';
 import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 import "../Profile/style.css";
 import UserCard from '../Profile/userCard.jsx';
 import OtherCard from '../Profile/otherDetails.jsx';
+import {BounceLoader} from 'react-spinners';
+import { docQr } from '../../Logics/docQr';
+import {useNavigate} from 'react-router-dom'
 const Profile = () => {
+  const userId = sessionStorage.getItem("uid");
+  const [user, setUser] = useState();
+  const navigate=useNavigate()
+  useEffect(() => {
+    if(!userId)return navigate('/');
+    const getUser = async () => {
+      const user = await docQr("Users", {
+        max: 1,
+        whereClauses: [
+          {
+            field: "uid",
+            operator: "==",
+            value: userId
+          }
+        ]
+      });
+      if(user.length > 0)setUser(user[0]);
+    }
+
+    getUser();
+  }, []);
   return (
-    <div style={{padding:18}}>
+    <div style={{ padding: 18 }}>
       <h3 style={{ padding: 10, fontWeight: "bolder" }}>Profile Dashboard</h3>
 
       <div className='dashboard'>
-      
-      <MDBContainer fluid >
-              <MDBRow style={{padding:0}}>
-                {/* Sidebar */}
 
-                <MDBCol md="5" lg="4"style={{padding:0}}>
-<UserCard user={user}/>
-</MDBCol>
+        <MDBContainer fluid >
+          <MDBRow style={{ padding: 0 }}>
+            {/* Sidebar */}
 
-
-                <MDBCol md="6" lg="8" className='otherCard'>
-<div>
-<OtherCard user={user}/>
-</div>
-                    </MDBCol>
+          {user ?  <MDBCol md="5" lg="4" style={{ padding: 0 }}>
+              <UserCard user={user} />
+            </MDBCol>:<BounceLoader/>}
 
 
-                    </MDBRow>
+            <MDBCol md="6" lg="8" className='otherCard'>
+            {user && <div>
+                <OtherCard user={user} />
+              </div>}
+            </MDBCol>
+
+
+          </MDBRow>
 
 
 
 
-                    </MDBContainer>
-      
+        </MDBContainer>
+
       </div>
     </div>
   );
